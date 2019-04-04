3aea0481c7ccca927531776ebefc37e7b04f9ba5
Nino3103 <jeangilardo@gmail.com>
2019-04-04 15:41:33 +0200

    ajout de l'exo password bien joué Romain git status! wesh il a dit bonjour

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,8 +1,28 @@
-puts "Donne un chiffre"
-chiffre = gets.to_i
-
-
-chiffre.downto(0) do
-  puts "* " * chiffre
-  chiffre -= 1
+def signup
+    puts "Définir votre mot de passe"
+    print ">"
+    mdp = gets.chomp.to_s
 end
+
+
+def login(mdp)
+    puts "Entrez votre mot de passe"
+    print ">"
+    essai_mdp = gets.chomp.to_s
+    if (essai_mdp != mdp)
+        puts "C’est pas le bon mot de passe wesh"
+    else welcome_screen
+    end
+end
+
+def welcome_screen
+    puts "Bienvenue dans ta zone secrète"
+    puts "Macron est un reptilien et Mickael Jackson n’est pas mort"
+end
+
+def perform
+    mdp = signup
+    login(mdp)
+end
+
+perform
