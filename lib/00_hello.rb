4c52ae5ace5ce9fdbae664f50be69f01fd64fb58
Ramoin <rgenestre@hotmail.com>
2019-04-04 12:46:29 +0200

    exo 1 fini

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,1 +1,18 @@
-puts "test"
+def ask_first_name
+	puts "C'est quoi ton blaze ?"
+	print ">"
+	first_name = gets.chomp 
+end
+
+def say_hello(argument)
+	puts "Bonjour, #{argument}!"
+end
+
+def perform
+	variable = ask_first_name
+	say_hello (variable)
+end
+
+perform
+
+ 
